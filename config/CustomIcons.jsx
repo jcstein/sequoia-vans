e40517bb8ad88b2536bae3ddf7d8c89f97c5fbd5
jcstein <mail@jcstein.dev>
2021-05-30 22:36:27 -0400

    fix icon and update coloring of header

--- config/CustomIcons.jsx
+++ config/CustomIcons.jsx
@@ -1,7 +1,7 @@
 import makeFAIcon from "utils/makeFAIcon";
 
-import { faVanShuttle, faLaptop, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faLaptop, faLock } from "@fortawesome/free-solid-svg-icons";
 
-export const ShoppingCartIcon = makeFAIcon(faVanShuttle);
+export const ShoppingCartIcon = makeFAIcon(faShoppingCart);
 export const LaptopIcon = makeFAIcon(faLaptop);
 export const LockIcon = makeFAIcon(faLock);
